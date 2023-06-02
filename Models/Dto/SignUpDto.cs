66d8c081ae14d70d111940c12d0ebcdd699bcaa0
nayef-shuvo <nayef.shuvo@gmail.com>
2023-06-02 17:17:01 +0600

    CRUD operations

---
+++ Models/Dto/SignUpDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project4_1.Models.Dto
+{
+    public class SignUpDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        public Rank Rank { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project4_1.Models
+{
+    public class Teacher
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; } = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        public Rank Rank { get; set; }
+
+        public string Department { get; set; } = "CSE";
+
+    }
+}
